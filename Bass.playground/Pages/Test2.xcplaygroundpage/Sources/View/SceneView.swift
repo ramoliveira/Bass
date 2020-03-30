1aae3250f8b2323d5f0da94128d85a198d41fb37
Ramon Dias <dias.ramon07@hotmail.com>
2020-03-30 16:51:21 -0300

    Ballon image added

--- Bass.playground/Pages/Test2.xcplaygroundpage/Sources/View/SceneView.swift
+++ Bass.playground/Pages/Test2.xcplaygroundpage/Sources/View/SceneView.swift
@@ -7,13 +7,6 @@ public class SceneView: NSView {
         self.wantsLayer = true
         self.layer?.backgroundColor = CGColor(red: 0.95, green: 0.92, blue: 0.71, alpha: 1)
         
-//        if let image =  NSImage(named: "character.png") {
-//            let imageView = NSImageView(image: image)
-//            imageView.frame = NSRect(x: 0, y: 0, width: 300, height: 300)
-//            self.addSubview(imageView)
-//        }
-        
-//        let character = CharacterView(frame: NSRect(x: 0, y: 0, width: 300, height: 300))
         let character = Scene8CharacterCreator().create()
         self.addSubview(character)
     }
